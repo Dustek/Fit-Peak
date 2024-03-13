49a25fbe68e90ed9d2112b28cd3142357ef894e2
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-13 19:22:37 +0000

    Added Tailwind directives for each tailwind layers to main css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: "class",
     theme: {
       extend: {},
     },
