92faeab6e45d285a60ecfef4afd1d8e158318688
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-13 20:23:28 +0000

    sorted out run dev issues

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
  
-import Navbar from 'navbar.jsx'
+import Navbar from './Components/Navbar/navbar'
 
 import Card from './card.jsx'
  
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from "./App";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as bootstrap from 'bootstrap';
 
 ReactDOM.render(
     <React.StrictMode>
