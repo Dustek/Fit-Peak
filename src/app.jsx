2f875a7dcf44fbf4e953fc126e2b46dade24adfe
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-13 19:16:29 +0000

    Added plain navbar component; added tailwind dependencie; added talwind.config.js file

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from 'navbar.jsx'
 
 function App() {
     return (
         <div>
+            <Navbar></Navbar>
             <h1>Placeholder</h1>
         </div>
     );
