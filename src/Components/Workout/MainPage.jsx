5d5a2dedbf8e20507447164922ceb53b478e89a7
Vytas <v.venckas@gmail.com>
2024-03-14 20:39:48 +0000

    added a selection form

--- src/Components/Workout/MainPage.jsx
+++ src/Components/Workout/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import ExerciseCard from './ExerciseCard.jsx';
+import ExerciseSearchForm from './ExerciseSearchForm'; // Import the ExerciseSearchForm component
 const muscleImages = {
   biceps: 'bicep.jpeg'
 // abdominals:
@@ -22,11 +23,20 @@ const muscleImages = {
 // triceps
 };
 const MainPage = () => {
-  const [exercises, setExercises] = useState([]);
-  useEffect(() => {
-    const fetchExercises = async () => {
+    const [exercises, setExercises] = useState([]);
+  
+    useEffect(() => {
+      // Fetch exercises initially (assuming you want to show some exercises by default)
+      fetchExercises();
+    }, []);
+  
+    const fetchExercises = async (options = {}) => {
       try {
-        const response = await axios.get('https://api.api-ninjas.com/v1/exercises?muscle=biceps', {
+        const response = await axios.get('https://api.api-ninjas.com/v1/exercises', {
+          params: {
+            limit: 10, // Always limit to 10 results
+            ...options // Spread the options object to include any additional parameters
+          },
           headers: {
             'X-Api-Key': 'mgS819STNx1KpuKbeAkddCtzAHrj9WgTMEsAwevC'
           }
@@ -36,17 +46,25 @@ const MainPage = () => {
         console.error('Request failed:', error);
       }
     };
-    fetchExercises();
-  }, []);
-  return (
-    <>
-    <div className="main-page">
-      {exercises.map((exercise, index) => (
-        <ExerciseCard key={index} exercise={exercise} muscleImages={muscleImages}/>
-      ))}
-    </div>
-    <Link to="/store">Go to Store</Link>
-</>
-  );
-};
-export default MainPage;
+  
+    // Callback function to handle search
+    const handleSearch = (options) => {
+      fetchExercises(options);
+    };
+  
+    return (
+      <>
+        {/* Render ExerciseSearchForm component */}
+        <ExerciseSearchForm onSearch={handleSearch} />
+  
+        <div className="main-page">
+          {exercises.map((exercise, index) => (
+            <ExerciseCard key={index} exercise={exercise} muscleImages={muscleImages} />
+          ))}
+        </div>
+        <Link to="/store">Go to Store</Link>
+      </>
+    );
+  };
+  
+  export default MainPage;
