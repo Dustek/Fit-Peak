043592c64c22423cf45af6ef22f8749004429b31
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-14 20:30:58 +0000

    Added navbar using a framer motion technologie

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,35 +1,49 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import './navbar.css';
+import React, { useState } from "react";
+import NavbarItems from "./NavbarItems";
+import { motion, AnimatePresence } from "framer-motion";
 
-function Navbar () {
-    return (
-        <>
-        <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Fit-Peak</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Exercise</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">Buy equipment</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">Messege us!</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-      </>
-    )
-}
+const Navbar = () => {
+  const [isToggled, setToggle] = useState(false);
+
+  const navContainer = {
+    visible: {
+      //x: 0,
+      opacity: 1,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.3
+      }
+    },
+    hidden: {
+      //x: -250,
+      opacity: 0,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.3
+      }
+    }
+  };
+
+  return (
+    <>
+      <button className="btn" onClick={() => setToggle(!isToggled)}>
+        =
+      </button>
+      <AnimatePresence>
+        {isToggled && (
+          <motion.div
+            className="navbar"
+            initial="hidden"
+            animate={isToggled ? "visible" : "hidden"}
+            exit="hidden"
+            variants={navContainer}
+          >
+            <NavbarItems isToggled={isToggled} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
 export default Navbar;
