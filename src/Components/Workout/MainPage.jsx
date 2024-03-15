99b89a6cc44b06b8aded1d12a7e496ac7c2a583a
Vytas <v.venckas@gmail.com>
2024-03-15 19:50:14 +0000

    fixed imports

--- src/Components/Workout/MainPage.jsx
+++ src/Components/Workout/MainPage.jsx
@@ -1,67 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import ExerciseCard from './ExerciseCard.jsx';
-import ExerciseSearchForm from './ExerciseSearchForm'; // Import the ExerciseSearchForm component
+import ExerciseCard from './ExerciseCard';
 const muscleImages = {
-  biceps: 'bicep.jpeg'
-// abdominals:
-// abductors:
-// adductors:
-// biceps
-// calves
-// chest
-// forearms
-// glutes
-// hamstrings
-// lats
-// lower_back
-// middle_back
-// neck
-// quadriceps
-// traps
-// triceps
+  biceps: 'bicep.jpeg',
+  // Add other muscle images as needed
 };
 const MainPage = () => {
-    const [exercises, setExercises] = useState([]);
-  
-    useEffect(() => {
-      // shows initial excercises
-      fetchExercises();
-    }, []);
-  
-    const fetchExercises = async (options = {}) => {
-      try {
-        // Log the request details before making the API call
-    console.log('API Request URL:', 'https://api.api-ninjas.com/v1/exercises');
-    console.log('API Request Parameters:', {
-      limit: 10,
-      ...options
-    });
-        const response = await axios.get('https://api.api-ninjas.com/v1/exercises', {
-          params: {
-            limit: 10, // Always limit to 10 results
-            ...options // Spread the options object to include any additional parameters
-          },
-          headers: {
-            'X-Api-Key': 'mgS819STNx1KpuKbeAkddCtzAHrj9WgTMEsAwevC'
-          }
-        });
-        setExercises(response.data);
-      } catch (error) {
-        console.error('Request failed:', error);
-      }
-    };
-  
-  // Callback function to handle search
-  const handleSearch = (options) => {
-    fetchExercises(options);
+  const [exercises, setExercises] = useState([]);
+  const [selectedMuscle, setSelectedMuscle] = useState('');
+  const [selectedType, setSelectedType] = useState('');
+  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get('https://api.api-ninjas.com/v1/exercises', {
+        params: {
+          difficulty: selectedDifficulty
+        },
+        headers: {
+          'X-Api-Key': 'mgS819STNx1KpuKbeAkddCtzAHrj9WgTMEsAwevC'
+        }
+      });
+      setExercises(response.data);
+    } catch (error) {
+      console.error('Request failed:', error);
+    }
   };
-
   return (
     <>
-      <ExerciseSearchForm onSearch={handleSearch} />
+      <div className="filter-options">
+        <label>
+          Difficulty Level:
+          <select value={selectedDifficulty} onChange={(e) => setSelectedDifficulty(e.target.value)}>
+            <option value="">Select Difficulty Level</option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="Expert">Expert</option>
+            {/* Options for difficulty levels */}
+          </select>
+        </label>
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <div className="main-page">
+        {/* Display filtered exercises */}
         {exercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} muscleImages={muscleImages} />
         ))}
@@ -70,5 +51,4 @@ const MainPage = () => {
     </>
   );
 };
-
 export default MainPage;
