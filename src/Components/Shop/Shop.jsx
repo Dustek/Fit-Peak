dab00710c1c26fe622c446be3bc800522a97f0a0
Lotus <niroshifinance@gmail.com>
2024-03-17 21:22:09 +0000

    comments are added to Shop components

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -5,28 +5,43 @@ import productsData from './productsData.json';
 
 import './Shop.css';
 
+
+//usestate is used to set the cart and get the cart from local storage
+//use effect will use for mount and unmount of the component(adds and removes items from the cart)
 const Shop = () => {
   const [cart, setCart] = useState([]);
+  //cart is an array of objects, each object is a product with a name, price, and image
+  //here its set to an empty array, setCart is a function that updates the cart and set  the cart in local storage
 
   useEffect(() => {
     // Load cart from local storage on component mount
     const storedCart = localStorage.getItem('cart');
+    //if there is a cart  in local storage, get the cart from the  local storage and set the cart to the cart in local storage
+    // [] means it will only run once when the component mounts and unmounts
+    //it will tell the react that your component needs to do something after render
+    
     if (storedCart) {
       setCart(JSON.parse(storedCart));
     }
   }, []);
 
+  //this useeffect will update the local storage with the cart whenever the cart changes
+  //now the [card] means this array will  tell the react that your component needs to do something after render
   useEffect(() => {
     // Update local storage whenever cart changes
     localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
 
   
-
+//add to cart function
+//[...cart, product] is a new array that contains all the items from the old cart and the new product 
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
 
+  //remove from cart function
+  //filter method will remove the item from the cart
+  //(_, i) in _ means we are not using the first parameter it reopresents the current item in the array and i is the index of the current item
   const removeFromCart = (index) => {
     const newCart = cart.filter((_, i) => i !== index);
     setCart(newCart);
