b02daa6fa5f39126ca13dea20dcfc9481afb7155
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-17 12:49:56 +0000

    Added basic code for footer; added icons for twitter, facebook, linkedin etc; installed bootstrap icons

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as bootstrap from 'bootstrap';
 import { motion } from "framer-motion"
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
