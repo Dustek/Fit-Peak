26358c85aaf016a05eef4823e4157a8fd5e74ba0
Lotus <niroshifinance@gmail.com>
2024-03-17 22:14:30 +0000

    comments are added for Workout Component

--- src/Components/Workout/ExerciseCard.jsx
+++ src/Components/Workout/ExerciseCard.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ExerciseCard.css';
 
+// ExerciseCard component is a child component of MainPage
+// It receives the exercise object and muscleImages object as props
+// It displays the exercise details in a card format
 const ExerciseCard = ({ exercise, muscleImages }) => {
   const { name, type, muscle, equipment, difficulty } = exercise;
 
+  //when the user clicks on the view instructions link, the selected exercise will be stored in the local storage
   const handleClick = () => {
     localStorage.setItem('selectedExercise', name);
   };
 
   return (
     <div className="exercise-card">
+      {/* muscleImages[muscle] means obj{key}  */}
     <img className="exercise-image" src={muscleImages[muscle]} alt={muscle} />
     <div className="exercise-details">
       <h2 className="exercise-name">{name}</h2>
