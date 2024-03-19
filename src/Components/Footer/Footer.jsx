1b3d0f94faea04052d688adeb7a05c956516adea
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-19 18:55:44 +0000

    Made footer smaller, added sticky postion to footer; edited size of icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,28 +5,28 @@ const Footer = () =>
 <div id="page-container">
 <div id="content-wrap">
 <footer className="footer--pin fixed-bottom text-center font-small" style={{width: '100%'}}>
-  <div className="container p-4 pb-0">
+  <div className="container p-2 pb-0">
       <div className="container-fluid">
-        <div className="mb-4">
-          <a className="fb-ic fa-lg white-text mr-md-5 mr-3 fa-2x" href="https://en-gb.facebook.com" style = {{ color: 'rgba(26, 225, 232)', margin: '20px'}}>
+        <div className="sm-3">
+          <a className="fb-ic fa-xs white-text mr-md-5 mr-3 fa-sm" href="https://en-gb.facebook.com" style = {{ color: 'rgba(26, 225, 232)', margin: '20px'}}>
             <MDBIcon icon='facebook'/>
           </a>
-          <a className="tw-ic fa-lg white-text mr-md-5 mr-3 fa-2x" href="https://twitter.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
+          <a className="tw-ic fa-lg white-text mr-md-5 mr-3 fa-sm" href="https://twitter.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
           <MDBIcon icon='twitter'/> 
           </a>
-          <a className="gplus-ic fa-lg white-text mr-md-5 mr-3 fa-2x" href="https://github.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
+          <a className="gplus-ic fa-lg white-text mr-md-5 mr-3 fa-sm" href="https://github.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
           <MDBIcon icon='github'/>
           </a>
-          <a className="li-ic fa-lg white-text mr-md-5 mr-3 fa-2x" href="https://linkedin.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
+          <a className="li-ic fa-lg white-text mr-md-5 mr-3 fa-sm" href="https://linkedin.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
           <MDBIcon icon='linkedin'/>
           </a>
-          <a className="ins-ic fa-lg white-text mr-md-5 mr-3 fa-2x" href="https://instagram.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
+          <a className="ins-ic fa-lg white-text mr-md-5 mr-3 fa-sm" href="https://instagram.com" style = {{ color: "rgba(26, 225, 232)", margin: '20px'}}>
           <MDBIcon icon='instagram'/>
           </a>
         </div>
     </div>
   </div>
-  <div className="footer-copyright text-center py-3" style={{color: "black"}}>© 2024 Copyright:
+  <div className="footer-copyright text-center " style={{color: "black"}}>© 2024 Copyright:
     <a href="/" style = {{ color: "rgba(26, 225, 232)", textDecoration: 'none'}}> TEAM 18</a>
   </div>
 </footer>
