caeaa1815c4d510a6ff271e15ab28f37390990be
Vytas <v.venckas@gmail.com>
2024-03-19 21:22:45 +0000

    added text expander, and changed some styles

--- src/Components/Workout/InstructionPage.jsx
+++ src/Components/Workout/InstructionPage.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import './InstructionPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faDumbbell,
+  faRunning,
+  faBurn,
+  faWeightHanging,
+  faIndustry,
+} from '@fortawesome/free-solid-svg-icons';
 
-
-
-
-// Assuming you have a directory of muscle images
 const InstructionPage = () => {
   const { exerciseName } = useParams();
   const [exercise, setExercise] = useState(null);
+  const [showFullInstructions, setShowFullInstructions] = useState(false);
+
   useEffect(() => {
     const fetchExercise = async () => {
       try {
@@ -25,12 +31,28 @@ const InstructionPage = () => {
     };
     fetchExercise();
   }, [exerciseName]);
-  // Get the selected exercise name from local storage
-  //const storedExerciseName = localStorage.getItem('selectedExercise');
-  // Mapping of exercise names to video file paths
+
+  const getEquipmentIcon = (equipment) => {
+    switch (equipment) {
+      case 'barbell':
+        return faWeightHanging;
+      case 'body_only':
+        return faRunning;
+      case 'kettlebells':
+        return faBurn;
+      case 'dumbbell':
+        return faDumbbell;
+      case 'other':
+        return faRunning;
+      case 'machine':
+        return faIndustry;
+      default:
+        return null;
+    }
+  };
+
   const videoPaths = {
     'Rickshaw Carry': '/beg1.mp4',
-    
     'Single-Leg Press': '/Single-Leg Press.mp4',
     'Clean from Blocks': '/block.mp4',
     'Incline Hammer Curls ': '/hammercurl.mp4',
@@ -44,19 +66,25 @@ const InstructionPage = () => {
     'Barbell forward lunge': '/barbell.mp4.mp4',
     // Add more exercise names and corresponding video file paths as needed
   };
+
+  const toggleInstructions = () => {
+    setShowFullInstructions(!showFullInstructions);
+  };
+
   return (
     <div className="instruction-page">
       {exercise ? (
-
-        <>
         <div className="main">
           <h2>{exercise.name}</h2>
           <p>Type: {exercise.type}</p>
-          <p>Equipment: {exercise.equipment}</p>
-          {/* Display muscle image */}
+          <p>
+            Equipment: {exercise.equipment}
+            {getEquipmentIcon(exercise.equipment) && (
+              <FontAwesomeIcon icon={getEquipmentIcon(exercise.equipment)} className="equipment-icon" />
+            )}
+          </p>
           <p>Difficulty: {exercise.difficulty}</p>
           <div className="video-container">
-            {/* Use HTML5 video element to display the video */}
             <video width="560" height="315" controls>
               <source src={videoPaths[exercise.name]} type="video/mp4" />
               Your browser does not support the video tag.
@@ -64,15 +92,23 @@ const InstructionPage = () => {
           </div>
           <div className="instructions">
             <h3>Instructions:</h3>
-            <p>{exercise.instructions}</p>
+            <p>
+              {showFullInstructions ? exercise.instructions : `${exercise.instructions.slice(0, 500)}...`}
+              {!showFullInstructions && (
+                <button className="read-more-button" onClick={toggleInstructions}>Show more</button>
+              )}
+              {showFullInstructions && (
+                <button className="less-button" onClick={toggleInstructions}>Show less</button>
+              )}
+            </p>
           </div>
-          </div>
-        </>
+          <Link to="/" className="back-button">Back to Main Page</Link>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
     </div>
- 
   );
 };
+
 export default InstructionPage;
